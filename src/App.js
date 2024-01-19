1ffa9a340ff063bf1431e77178bb79bd4aaf8e7b
Simone Trevisan <simone.trevisan@ibc.srl>
2024-01-19 09:01:59 +0100

    first commit my site

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Benvenuto sul sito di Simone Trevisan!</h1>
+    </>
+  );
+}
+
+export default App;
