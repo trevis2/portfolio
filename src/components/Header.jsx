5d25b6732c1e25b5a708a4a2147fdc1a93afa934
Simone Trevisan <simone.trevisan@ibc.srl>
2024-01-19 23:15:48 +0100

    add sharp href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ const Header = () => {
                 <img src="logo200.png" />
             </div>
             <div className='navbar'>
-                <a href=''>Profilo</a>
-                <a href=''>Link2</a>
-                <a href=''>Link3</a>
-                <a href=''>Link4</a>
-                <a href=''>Link5</a>
-                <a href=''>Link6</a>
+                <a href='#'>Profilo</a>
+                <a href='#'>Link2</a>
+                <a href='#'>Link3</a>
+                <a href='#'>Link4</a>
+                <a href='#'>Link5</a>
+                <a href='#'>Link6</a>
             </div>
             <div className='menu'>
                 download icon
