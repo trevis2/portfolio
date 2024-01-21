d6eb9b668b2a684dde2b46da121772e76f90d266
Simone Trevisan <simone.trevisan@ibc.srl>
2024-01-21 11:48:50 +0100

    add env variable and WIP Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import './App.css';
 import { Navbar } from './components';
 import { Header, About, Skills, Testimonials, Work, Footer } from './features';
+import { FcVlc } from "react-icons/fc";
 
 
 function App() {
+  const wip = process.env.REACT_APP_WORK_IN_PROGRESS;
   return (
     <div className='app'>
-      <Navbar />
-      <Header />
-      <About />
-      <Skills />
-      <Testimonials />
-      <Work />
-      <Footer />
+      {
+        !wip &&
+        <>
+          <Navbar />
+          <Header />
+          <About />
+          <Skills />
+          <Testimonials />
+          <Work />
+          <Footer />
+        </>
+      }
+      {
+        wip && <div>
+          <div>
+            <FcVlc size={30} />
+            <FcVlc size={30} />
+            <b style={{ fontSize: 25 }}>Work in progress!!</b>
+            <FcVlc size={30} />
+            <FcVlc size={30} />
+          </div>
+          <br />
+          <br />
+          <h1>Welcome in the Simone Trevisan's professional blog</h1>
+          <h2>the site is under construction...</h2>
+          <br />
+          <br />
+          <br />
+          <h4>If you need to contact me, send me an email to trevis2@gmail.com</h4>
+          <br />
+          <h4>Bye!</h4>
+
+        </div>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
