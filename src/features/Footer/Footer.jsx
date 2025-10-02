0b70925cd0ea7c5bda94ba1081ba864074ecc321
Simone Trevisan <trevis2@gmail.com>
2025-10-02 14:15:30 +0200

    fix footer

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () => {
           <p>Game Programmer</p>
         </div>
         
+        {/* 
         <div className="app__footer-links">
           <a href="#home">Home</a>
           <a href="#about-me">Chi Sono</a>
@@ -18,7 +19,8 @@ const Footer = () => {
           <a href="#portfolio">Progetti</a>
           <a href="#contact-info">Contatti</a>
         </div>
-        
+        */}
+
         <div className="app__footer-social">
           <a href="https://www.linkedin.com/in/simone-t-99ab2265/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
             <FaLinkedin />
